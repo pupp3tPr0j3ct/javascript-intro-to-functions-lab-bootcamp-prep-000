4f3cc3cbfd0704a2e5c0d083d4a18cebaccea02a
Jason Santana <jsantana006@gmail.com>
2019-08-18 00:28:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function sayHiToGrandma(string){
     } else
     if(string.toLowerCase()){
       return "I can't hear you!";
-    } else 
-    if(string === "I love you, Grandma."){
-      return "I love you, too.";
-    }
-}
+      } else 
+      if(string === "I love you, Grandma."){
+        return "I love you, too.";
+      }
+  }
 
